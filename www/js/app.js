7d1ba4619480aa9296ea2d758b9b10a00343985d
Jesse Air <jburcsik@kivuto.com>
2013-12-07 12:49:19 -0500

    Added jquery source file because the build was not working with the CND version

--- www/js/app.js
+++ www/js/app.js
@@ -5,8 +5,7 @@
 requirejs.config({
     "baseUrl": "js/lib",
     "paths": {
-      "app": "../app",
-      "jquery": "//ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min"
+      "app": "../app"
     }
 });
 
