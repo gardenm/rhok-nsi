8b8916e104fac33d44b45268ccee987e1002c797
Matthew Garden <matthew.garden@lightspeedretail.com>
2013-12-07 15:12:32 -0500

    Added a mercator map.

--- www/js/app/main.js
+++ www/js/app/main.js
@@ -1,9 +1,10 @@
-define(["jquery", "d3.min", "d3test", "matt"], function($, ignore, d3test, matt) {
+define(["jquery", "d3.min", "d3test", "matt", "topojson.min"], function($, ignore, d3test, matt) {
     //the jquery.alpha.js and jquery.beta.js plugins have been loaded.
     $(function() {
 
         d3test.init();
 
         matt.addCircles();
+        matt.addMercator();
     });
 });
