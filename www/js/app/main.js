ebf0b0d0202ac3b803bcef3c55a1d275f3620610
Jesse Air <jburcsik@kivuto.com>
2013-12-07 13:31:22 -0500

    adding dribble

--- www/js/app/main.js
+++ www/js/app/main.js
@@ -1,7 +1,7 @@
-define(["jquery", "d3.min","jquery.alpha", "jquery.beta"], function($, ignore) {
+define(["jquery", "d3.min","d3test", "jquery.beta"], function($, ignore, d3test) {
     //the jquery.alpha.js and jquery.beta.js plugins have been loaded.
     $(function() {
-        $('body').alpha().beta();
+        $('body').d3test().beta();
         console.log(d3);
     });
 });
---
+++ www/js/lib/d3test.js
@@ -0,0 +1,9 @@
+define(["jquery"], function($) {
+  
+  $.fn.d3test = function() {
+      return this.append('<p>d3 Test Content</p>');
+  };
+    
+    
+    
+});
