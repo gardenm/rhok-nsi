36857f29fab2be2f1f4a19efc4fba2a66c1f7eb2
Jesse Air <jburcsik@kivuto.com>
2013-12-07 14:20:15 -0500

    removed jquery.beta

--- www/js/lib/d3test.js
+++ www/js/lib/d3test.js
@@ -1,9 +1,22 @@
 define(["jquery"], function($) {
   
-  $.fn.d3test = function() {
-      return this.append('<p>d3 Test Content</p>');
-  };
-    
-    
-    
-});
+	 var init = function(){	 
+		
+		var data = [4, 8, 15, 16, 23, 42];
+		
+		d3.select('body').append('div').attr('class','chart');
+		
+		d3.select(".chart")
+		  .selectAll("div")
+		    .data(data)
+		  .enter().append("div")
+		    .style("width", function(d) { return d * 10 + "px"; })
+		    .text(function(d) { return d; });
+		
+	 };
+	 
+	 return{
+	 	'init':init
+	 };
+
+  });
