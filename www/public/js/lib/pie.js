06003a9018695b2c4a5d8cdfc5de4bde2d6ec95d
Matthew Garden <matthew.garden@lightspeedretail.com>
2013-12-08 13:13:15 -0500

    Add a white outline to pie charts on the map.

--- www/public/js/lib/pie.js
+++ www/public/js/lib/pie.js
@@ -1,32 +1,40 @@
 
 define(["jquery"], function ($) {
     var pieChart = function(precentages, size, object, showText) {
-      var h = size;
-      var w = size;
+        var w = size;
+        var border = 2;
 
-      var dataset = precentages;
+        var dataset = precentages;
 
-      var pie = d3.layout.pie();
+        var pie = d3.layout.pie();
 
-      var outerRadius = w / 2;
-      var innerRadius = 0;
-      var arc = d3.svg.arc()
-        .innerRadius(innerRadius)
-        .outerRadius(outerRadius);
+        var outerRadius = w / 2;
+        var innerRadius = 0;
+        var arc = d3.svg.arc()
+            .innerRadius(innerRadius)
+            .outerRadius(outerRadius);
 
-      var arcs = object.selectAll("g.arc")
-        .data(pie(dataset))
-        .enter()
-        .append("g")
-        .attr("class", "arc")
-        .attr("transform", "translate(" + outerRadius + ", " + outerRadius + ")");
-    
-      arcs.append("path")
-          .attr("class", function (d, i) {
-              if (i === 0) return "spent";
-              else return "remaining";
-          })
-          .attr("d", arc);
+        var arcBorder = d3.svg.arc()
+            .innerRadius(outerRadius)
+            .outerRadius(outerRadius + border);
+
+        var arcs = object.selectAll("g.arc")
+            .data(pie(dataset))
+            .enter()
+            .append("g")
+            .attr("class", "arc")
+            .attr("transform", "translate(" + (outerRadius + border) + ", " + (outerRadius + border) + ")");
+
+         arcs.append("path")
+           .attr("class", function (d, i) {
+               if (i === 0) return "spent";
+               else return "remaining";
+           })
+           .attr("d", arc);
+
+        arcs.append("path")
+            .attr("fill", "white")
+            .attr("d", arcBorder);
 
       if (showText) {
           arcs.append("text")
